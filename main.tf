cfd0496ea4f13d928e9bd25810e2964e5ef3bb30
Jeremiah Harlan <jeremiah.harlan@gmail.com>
2019-08-13 13:14:26 -0600

    task: update terraform label module

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 # Define composite variables for resources
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.6"
-  namespace  = "${var.namespace}"
-  name       = "${var.name}"
-  stage      = "${var.stage}"
-  delimiter  = "${var.delimiter}"
-  attributes = "${var.attributes}"
-  tags       = "${var.tags}"
+  source     = "git::https://github.com/pgalchemy/terraform-null-label.git?ref=v0.15.0"
+  namespace  = var.namespace
+  name       = var.name
+  stage      = var.stage
+  delimiter  = var.delimiter
+  attributes = var.attributes
+  tags       = var.tags
 }
 
 data "aws_region" "default" {}
